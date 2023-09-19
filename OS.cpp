663c6681a68f1fcf8628db4c25d5aa9509143f18
Andrii Myrosh <130073847+AwfulIceCream@users.noreply.github.com>
2023-09-19 14:00:22 +0300

    Add files via upload

---
+++ OS.cpp
@@ -0,0 +1,57 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+#include <psapi.h>
+
+using namespace std;
+
+void MonitorProcessMemoryUsage(DWORD processId, SIZE_T maxWorkingSetSize)
+{
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+	if (hProcess == NULL)
+	{
+		cout << "Failed to open process (" << GetLastError() << ")." << endl;
+		return;
+	}
+
+	while (true)
+	{
+		Sleep(5000);
+
+		PROCESS_MEMORY_COUNTERS pmc;
+		if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc)))
+		{
+			SIZE_T workingSetSize = pmc.WorkingSetSize;
+
+			if (workingSetSize > maxWorkingSetSize)
+			{
+				cout << "Memory limit exceeded. Terminating process." << endl;
+				TerminateProcess(hProcess, 1);
+				CloseHandle(hProcess);
+				break;
+			}
+			else
+			{
+				cout << "Memory usage is within limits." << endl;
+			}
+		}
+
+		Sleep(1000);
+	}
+}
+
+int main()
+{
+	DWORD processId;
+	SIZE_T maxWorkingSetSize;
+
+	cout << "Enter the Process ID to monitor: ";
+	cin >> processId;
+
+	cout << "Enter the maximum working set size (in bytes): ";
+	cin >> maxWorkingSetSize;
+
+	MonitorProcessMemoryUsage(processId, maxWorkingSetSize);
+
+	return 0;
+}
